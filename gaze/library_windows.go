cfdcbcbd4190988b854c6da03368da7a3d089edb
Zephyyrr <zapmew@gmail.com>
2014-02-19 16:56:25 +0100

    Cleaned up code remains from eyex era

--- gaze/library_windows.go
+++ gaze/library_windows.go
@@ -8,35 +8,35 @@ import "C"
 import (
 	"fmt"
 	"syscall"
-	"unsafe"
+	//"unsafe"
 )
 
 const (
 
 	//tobiigaze.h
-	tobiigaze_create = iota
-	tobiigaze_destroy
-	tobiigaze_connect
-	tobiigaze_disconnect
-	tobiigaze_run_event_loop
-	tobiigaze_break_event_loop
-	tobiigaze_start_tracking
-	tobiigaze_stop_tracking
-	tobiigaze_get_device_info
-	tobiigaze_get_track_box
-	tobiigaze_get_url
-	tobiigaze_is_connected
-	tobiigaze_get_error_message
+	create = iota
+	destroy
+	connect
+	disconnect
+	run_event_loop
+	break_event_loop
+	start_tracking
+	stop_tracking
+	get_device_info
+	get_track_box
+	get_url
+	is_connected
+	get_error_message
 
 	//tobiigaze_discovery.h
-	tobiigaze_list_usb_eye_trackers
-	tobiigaze_get_connected_eye_tracker
+	list_usb_eye_trackers
+	get_connected_eye_tracker
 
 	// not a function
 	lastIndex
 )
 
-const eyexName = `TobiiGazeCore64.dll`
+const dllName = `TobiiGazeCore64.dll`
 
 var (
 	txFunc = make([]callable, lastIndex, lastIndex)
@@ -74,40 +74,35 @@ func abort(funcname string, err error) {
 }
 
 func wInitializeSystem() {
-	//txInitializeSystem(TX_SYSTEMCOMPONENTOVERRIDEFLAG_NONE, NULL, NULL)
-	ret, _, callErr := txFunc[txInitializeSystem].Call(
-		txSystemComponentOverrideFlagNone,
-		0, // null
-		0) // null
-
-	result := txResult(ret)
-
-	if result != txResultOk {
-		abort(txName[txInitializeSystem], callErr)
-	}
 
 }
 
 func wCreateContext(something bool) (uintptr, error) {
-	var handle uintptr = TxEmptyHandle
-	pointer := uintptr(unsafe.Pointer(&handle)) //**void
-
-	ret, _, _ := txFunc[txCreateContext].Call(
-		pointer,
-		txTrue)
-
-	result := txResult(ret)
-	if result == txResultOk {
-		return handle, nil
-	}
-
-	return 0, result
+	return 0, nil
 }
 
 func wReleaseContext(handle uintptr) error {
-
+	return nil;
 }
 
 func init() {
+	var err error
 
+	// Hack to make Windows report
+	// what caused a LoadDLL failure
+	C.SetErrorMode(0)
+
+	gaze, err := syscall.LoadDLL(dllName)
+
+	if err != nil {
+		abort("Failed to load "+dllName, err)
+	}
+
+	for i, name := range txName {
+		txFunc[i], err = gaze.FindProc(name)
+
+		if err != nil {
+			abort("Loading Tobii Gaze function "+name, err)
+		}
+	}
 }
