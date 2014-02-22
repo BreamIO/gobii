f4db888964923cc1f8879638d321c7bfa1540534
Oskar Segersvärd <victorystick@gmail.com>
2014-02-22 13:41:47 +0100

    Switched to using AnyConnectedEyeTracker

--- examples/simpleGazeReader/main.go
+++ examples/simpleGazeReader/main.go
@@ -3,26 +3,28 @@ package main
 import (
 	"fmt"
 	"time"
-	tobii "github.com/zephyyrr/gobii"
+	tobii "github.com/zephyyrr/gobii/gaze"
 )
 
 func main() {
-	et, err := tobii.NewEyeTracker()
-	defer et.Close()
-	//err = et.onConnectionStateChanged();
+	fmt.Println("Attempting to find a connected EyeTracker...")
+	et, err := tobii.AnyConnectedEyeTracker()
+
 	if err != nil {
-		fmt.Println("Error:",err)
+		fmt.Println("Error:", err)
 		return
 	}
+
+	defer et.Close()
 	
 	fmt.Printf("Tracker: %#v\n", et)
-	gaze := et.GetGazeChannel();
+	// gaze := et.GetGazeChannel();
 	
-	go func(){
-		for point := range gaze {
-			fmt.Println(point);
-		}
-	}()
-	
+	// go func(){
+	// 	for point := range gaze {
+	// 		fmt.Println(point);
+	// 	}
+	// }()
+
 	time.Sleep(time.Second*30);
 }
