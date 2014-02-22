2bb0964cf2a17affb77f49d9809ece5b70179c4b
Oskar Segersvärd <victorystick@gmail.com>
2014-02-22 14:01:52 +0100

    Wrong commit. Fix here.

--- examples/simpleGazeReader/main.go
+++ examples/simpleGazeReader/main.go
@@ -2,19 +2,6 @@ package main
 
 import (
 	"fmt"
-<<<<<<< HEAD
-	"time"
-	tobii "github.com/zephyyrr/gobii/gaze"
-)
-
-func main() {
-	fmt.Println("Attempting to find a connected EyeTracker...")
-	et, err := tobii.AnyConnectedEyeTracker()
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-=======
 	"log"
 	//"time"
 	"github.com/zephyyrr/gobii/gaze"
@@ -29,22 +16,18 @@ func main() {
 	defer et.Close()
 	//err = et.onConnectionStateChanged();
 	if err != nil {
-		log.Fatalln("Error:",err)
->>>>>>> 82054268482a78533142b2de476a788fcf4f61d3
+		log.Fatalln("Error:", err)
 	}
 
 	defer et.Close()
 	
 	fmt.Printf("Tracker: %#v\n", et)
-<<<<<<< HEAD
-	// gaze := et.GetGazeChannel();
-=======
+
 	checked(et.Connect())
 	
 	//et.startTracking(func (data GazeData) {
 	//	fmt.Println(data)
 	//})
->>>>>>> 82054268482a78533142b2de476a788fcf4f61d3
 	
 	// go func(){
 	// 	for point := range gaze {
@@ -52,12 +35,11 @@ func main() {
 	// 	}
 	// }()
 
-	time.Sleep(time.Second*30);
-	*/
+	time.Sleep(time.Second*30)
 }
 
 func checked (err error) {
 	if err != nil {
-		log.Fatalln(err);
+		log.Fatalln(err)
 	}
 }
