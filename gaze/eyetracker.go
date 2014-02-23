bd9fd56080ef02429521bc5f4357678e53a1cc2f
Zephyyrr <zapmew@gmail.com>
2014-02-23 19:46:36 +0100

    Improved documentation. Added EyeTracker exmple.

--- gaze/eyetracker.go
+++ gaze/eyetracker.go
@@ -9,12 +9,43 @@ package gaze
 import "C"
 
 import (
+	"log"
 	"unsafe"
 )
 
 // An abstraction for tobiigaze_eye_tracker
+//
+// An eye tracker is not some children's toy.
+// It is a real piece of hardware.
+// We (and Tobii) do our best here to make sure stuff works, 
+// but you as a programmer need to understand that exceptions 
+// (or the nearest equivalent) is not really an exception.
+// Stuff will happen. Deal with it.
+//
+// With that being said.
+// Good Luck!
+
 type EyeTracker struct {
 	handle *C.tobiigaze_eye_tracker
+}
+
+// This is what you are really looking for.
+// The basic use of it is to first create one.
+// Then you connect to it using Connect.
+// At this point, defer a Close().
+// Then you need to StartTracking.
+// Give us a function of the TrackerFunc type and we handle the rest.
+func ExampleEyeTracker() {
+	et, err := AnyEyeTracker()
+	if err != nil {
+		log.Fatalln("AHHHH!!! ERROR!!!")
+		log.Fatalln(err)
+	}
+	et.Connect()
+	defer et.Close()
+	//et.StartTracking(func(data ETData) {
+	//	log.Println(data)
+	//})
 }
 
 // Get a C-style pointer
@@ -23,6 +54,7 @@ func (e EyeTracker) cPtr() *C.tobiigaze_eye_tracker {
 }
 
 // Creates a new EyeTracker instance from the given url.
+//
 // An error will be returned if there was an error.
 // The URL should have a format like:
 //   "tet-tcp://XXX.XXX.XXX.XXX"
@@ -44,6 +76,7 @@ func EyeTrackerFromURL(url string) (*EyeTracker, error) {
 }
 
 // Gets the URL of any connected EyeTracker.
+//
 // Otherwise returns an error.
 func AnyEyeTrackerURL() (string, error) {
 	const capacity uint32 = 256
@@ -75,7 +108,10 @@ func AnyEyeTracker() (*EyeTracker, error) {
 	return EyeTrackerFromURL(url)
 }
 
-// Attempt to connect to the physical eyetracker.
+// Attempt to connect to the physical eye tracker.
+//
+// Returns nil if everything went fine.
+// otherwise an Error.
 // Blocking function which may return an error.
 func (e EyeTracker) Connect() error {
 	var err Error
@@ -90,7 +126,8 @@ func (e EyeTracker) Connect() error {
 }
 
 // Closes the connection to the EyeTracker
-// Implements Closer interface
+//
+// Implements io.Closer interface
 func (e EyeTracker) Close() error {
 	if e.IsConnected() {
 		C.tobiigaze_disconnect(e.cPtr())
@@ -102,10 +139,16 @@ func (e EyeTracker) Close() error {
 }
 
 // Checks if the eye tracker has been connected.
+//
+// Returns true if it is connected.
+// False otherwise.
 func (e EyeTracker) IsConnected() bool {
 	return C.tobiigaze_is_connected(e.cPtr()) == 1
 }
 
+// A settable option for the eye tracker
+// 
+// Can be set using the "SetOption" method of EyeTrackers.
 type EyeTrackerOption C.tobiigaze_option
 
 const (
@@ -116,6 +159,9 @@ func (e EyeTrackerOption) cTyp() C.tobiigaze_option {
 	return (C.tobiigaze_option)(e)
 }
 
+// Allows you to set custom settings for the tracker.
+// This includes, but is not limited to the possibility to set
+// the timeout time before the synchronous operations timesout
 func (e EyeTracker) SetOption(o EyeTrackerOption, value int) error {
 	var err Error
 
@@ -144,11 +190,37 @@ func (e EyeTracker) URL() string {
 }
 
 // Go level abstraction for the device_info struct.
+//
+// The EyeTrackerInfo can be used to query some meta-data about the tracker.
 type EyeTrackerInfo C.struct_tobiigaze_device_info
 
 // Get a C pointer.
 func (e EyeTrackerInfo) cPtr() *C.struct_tobiigaze_device_info {
 	return (*C.struct_tobiigaze_device_info)(&e)
+}
+
+// Returns the serial number associated with this EyeTrackerInfo object.
+func (e EyeTrackerInfo) SerialNumber() string {
+	return C.GoString((*C.char)(&e.cPtr().serial_number[0]))
+}
+
+// Returns the eye trackers model
+func (e EyeTrackerInfo) Model() string {
+	return C.GoString((*C.char)(&e.cPtr().model[0]))
+}
+
+// Returns the generation of the eye tracker.
+//
+// Tobii's eye trackers have been through several "generations"
+// So this is basically a measure of how ancient the tracker is.
+func (e EyeTrackerInfo) Generation() string {
+	return C.GoString((*C.char)(&e.cPtr().generation[0]))
+}
+
+// Returns a string representing the version number of the firmware
+// running on the tracker.
+func (e EyeTrackerInfo) FirmwareVersion() string {
+	return C.GoString((*C.char)(&e.cPtr().firmware_version[0]))
 }
 
 // Gets a Go style EyeTrackerInfo object or an error
