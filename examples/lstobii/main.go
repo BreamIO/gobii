66d62474172ed43fa75f7756378055aad1fa2b63
Zephyyrr <zapmew@gmail.com>
2014-02-24 12:59:30 +0100

    Updated documentation. It is not a package, but a command.

--- examples/lstobii/main.go
+++ examples/lstobii/main.go
@@ -1,6 +1,6 @@
 //Copyright 2014 Bream IO AB. All rights reserved.
 
-// Simple packaged that demonstrates how to list connected eye trackers
+// Simple command that demonstrates how to list connected eye trackers
 //
 // Can be used as a debugging tool to detect what trackers is actually detected by the system
 // Only lists USB trackers and not TCP-over-USB trackers.
